e0cd513c14c40f9db885a776ab1faef2295dbc0a
JovanMarvel <jovanmarvel99@gmail.com>
2024-11-30 13:51:48 +0700

    Update script.js 1
    Fixed the code
    Includes numbers to f_ω (previously f_2)

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let t = 0;
+let baseFontSize = 7;
+
+let fontSize = baseFontSize;
+
 function baseFunction(n, a = 0){
   return Math.floor(10**10**n**1.229 + a).toLocaleString().replace(/,/g, " ");
 }
@@ -10,11 +15,19 @@ function findFontSize(){
   return document.getElementById("numberContainer").clientWidth / document.getElementById("num").clientWidth * fontSize;
 }
 
+function ban1(n){
+  if (n < 1) return baseFunction(n);
+  else {
+    let i = 10 ** (n % 1) ** 1.229;
+    return (n < 2 ? "10<sup>" : "{10, ").repeat(Math.floor(i)) + ban1((i % 1) * (n % 1 == 0 ? n - 1 : Math.floor(n))) + (n < 2 ? "</sup>" : ", " + Math.floor(n) + "}").repeat(Math.floor(i));
+  }
+}
+
 function getNum(){
   if(t < 3000){
     return baseFunction(t/3000, -10);
   } else {
-    return "10<sup>".repeat(Math.floor(t/3000)) + baseFunction((t / 3000) % 1) + "</sup>".repeat(Math.floor(t/3000));
+    return ban1(t/3000);
   }
 }
 
