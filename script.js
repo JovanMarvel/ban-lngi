73d6afb8d7f7253cfcf781b380c325bf2e538bf3
JovanMarvel <jovanmarvel99@gmail.com>
2024-12-12 17:28:14 +0700

    Update script.js 2
    Limit increase to f_ω2

--- script.js
+++ script.js
@@ -23,11 +23,19 @@ function ban1(n){
   }
 }
 
+function ban2(n){
+  if (n < 1) return ban1(preciseBaseFunction(n));
+  else {
+    let i = 10 ** (n % 1) ** 1.229;
+    return (n < 2 ? "{10, 10, " : "{10, ").repeat(Math.floor(i)) + ban2((i % 1) * (n % 1 == 0 ? n - 1 : Math.floor(n))) + (n < 2 ? "}" : ", " + Math.floor(n - 1) + ", 2}").repeat(Math.floor(i));
+  }
+}
+
 function getNum(){
   if(t < 3000){
     return baseFunction(t/3000, -10);
   } else {
-    return ban1(t/3000);
+    return ban2(t/3000);
   }
 }
 
